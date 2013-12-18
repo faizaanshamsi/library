0a5d5c1bcdc4de436d47ea9c1e5552ec58fc73cd
faizaanshamsi <faizaanshamsi@gmail.com>
2013-12-17 19:33:14 -0500

    add rating

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,4 +2,8 @@ require 'spec_helper'
 
 describe Book do
   it {should validate_presence_of(:title)}
+
+  it {should validate_numericality_of(:rating).is_greater_than_or_equal_to(0)}
+
+  it {should validate_numericality_of(:rating).is_less_than_or_equal_to(100)}
 end
